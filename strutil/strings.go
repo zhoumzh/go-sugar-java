11f13b798bb133ad62ab8623d953105c1367d3db
zhoumingzhu <zhoumingzhu@lixiang.com>
2022-07-01 16:10:31 +0800

    add strutil

---
+++ strutil/strings.go
@@ -0,0 +1,20 @@
+package strutil
+
+func Substring(source string, start int, end int) string {
+
+	var substring = ""
+	var pos = 0
+	for _, c := range source {
+		if pos < start {
+			pos++
+			continue
+		}
+		if pos >= end {
+			break
+		}
+		pos++
+		substring += string(c)
+	}
+
+	return substring
+}
