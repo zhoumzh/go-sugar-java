6b52802c5b0da1d9c16f188613a29eaa898a6797
zhoumingzhu <zhoumingzhu@lixiang.com>
2022-07-12 11:02:23 +0800

    add contains debug

--- collections/collection.go
+++ collections/collection.go
@@ -1,12 +1,16 @@
 package collections
 
-import "reflect"
+import (
+	"fmt"
+	"reflect"
+)
 
-func Contains(array interface{}, target interface{}) bool {
-	targetValue := reflect.ValueOf(array)
-	switch reflect.TypeOf(array).Kind() {
+func Contains(collection interface{}, target interface{}) bool {
+	targetValue := reflect.ValueOf(collection)
+	switch reflect.TypeOf(collection).Kind() {
 	case reflect.Slice, reflect.Array:
 		for i := 0; i < targetValue.Len(); i++ {
+			fmt.Println(targetValue.Index(i).Interface(), "==?", target)
 			if targetValue.Index(i).Interface() == target {
 				return true
 			}
